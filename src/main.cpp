b0ba1b156bb37c3cd0bc284fdb155a2985bce81d
rhoneybul <honeybulr@gmail.com>
2019-07-17 14:32:26 +0800

    added custom exception class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <exception>
+
+#include "customexception.h"
+#include "processor.h"
 
 using namespace std;
 
+// void do_something(bool failure, bool custom) {
+//     cout << "INFO::doing something::" << "failure::" << failure << "custom::" << custom << endl;
+//     if (failure) {
+//         if (custom) {
+//             throw CustomException();
+//         } else {
+//             throw exception();
+//         }
+        
+//     }
+// }
+
 int main() {
-    cout << "hello there, cruel world" << endl;
+    
+    cout << "Creating Processor" << endl;
+
+    Processor p;
+
+    // while()
+    try {
+        p.run();
+    } catch(CustomException& err) {
+        cout << "Processor Threw Error::" << err.what() << endl;
+    } catch(...) {
+        cout << "Processor Threw Error";
+    };
+    
     return 0;
 };
